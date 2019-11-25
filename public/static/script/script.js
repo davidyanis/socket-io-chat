ac4f4a8e476b9af978a2b86fb1d0aa39a11e984d
davidyanis <daveddd97@hotmail.com>
2019-11-25 20:27:34 +0100

    when typing print msg

--- public/static/script/script.js
+++ public/static/script/script.js
@@ -3,34 +3,65 @@ const socket = io();
 
 function sendChat(event) {
     event.preventDefault(); // prevents page reloading
-    const getMessage = document.getElementById("m").value;
-    socket.emit('chat message', getMessage);
+    const getMessage = document.getElementById("m");
+    socket.emit('chat message', getMessage.value);
+    getMessage.value = "";
+    return false;
 };
 
 
-    socket.on('chat message', function(msg){
-        console.log(msg)
-        const messageContainer = document.getElementById("messages");
-        const linkElement = document.createElement("li")
+socket.on('chat message', function(msg){
+    const messageContainer = document.getElementById("messages");
+    const linkElement = document.createElement("li")
 
-        linkElement.innerHTML = msg
-        messageContainer.appendChild(linkElement)
-    });
+    linkElement.innerHTML = msg
+    messageContainer.appendChild(linkElement)
+});
+
+socket.on('connected user', function() {
+    const messageContainer = document.getElementById("messages");
+    const linkElement = document.createElement("li")
+
+    linkElement.innerHTML = "Random user has connected."
+    messageContainer.appendChild(linkElement)
+});
+socket.on('disconnected user', function() {
+    const messageContainer = document.getElementById("messages");
+    const linkElement = document.createElement("li")
+
+    linkElement.innerHTML = "Random user has disconnected."
+    messageContainer.appendChild(linkElement)
+});
+
+socket.on('typing', function(typing){
+    const typingContainer = document.getElementById("typing");
+    if (typing) {
+        typingContainer.innerHTML = "Någon skriver...";
+    } else {
+        typingContainer.innerHTML = "";
+    }
+});
+
+var typing = false;
+var timeout = undefined;
+
+function timeoutFunction(){
+    typing = false;
+    socket.emit("typing", typing);
+}
+
+document.getElementById("m").addEventListener("input", function() {
+    if(typing == false) {
+        typing = true
+        socket.emit("typing", typing);
+        timeout = setTimeout(timeoutFunction, 3000);
+    } else {
+        clearTimeout(timeout);
+        timeout = setTimeout(timeoutFunction, 3000);
+    }
+})
     
-    socket.on('connected user', function() {
-        const messageContainer = document.getElementById("messages");
-        const linkElement = document.createElement("li")
 
-        linkElement.innerHTML = "Anononym user has connected."
-        messageContainer.appendChild(linkElement)
-    });
-    socket.on('disconnected user', function() {
-        const messageContainer = document.getElementById("messages");
-        const linkElement = document.createElement("li")
-
-        linkElement.innerHTML = "Anononym user has disconnected."
-        messageContainer.appendChild(linkElement)
-    });
 
 
 
