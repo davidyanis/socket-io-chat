6c8f0da6d76db3e5f215f8a1d4af89d19fecbbc9
davidyanis <daveddd97@hotmail.com>
2019-11-25 20:27:11 +0100

    when typing print msg

--- server.js
+++ server.js
@@ -11,20 +11,25 @@ app.get('/', function(req, res){
 });
 
 io.on('connection', function(socket){
-  console.log('a user connected');
+    console.log('a user connected');
 
-  socket.broadcast.emit('connected user');
+    socket.broadcast.emit('connected user');
 
 
-  socket.on('disconnect', function(){
-      console.log('user disconnected');
-      socket.broadcast.emit('disconnected user');
-  });
+    socket.on('disconnect', function(){
+        console.log('user disconnected');
+        socket.broadcast.emit('disconnected user');
+    });
 
-  socket.on('chat message', function(msg){
-      console.log('message: ' +msg);
-      io.emit('chat message', msg);
-  });
+    socket.on('chat message', function(msg){
+        console.log('message: ' +msg);
+        io.emit('chat message', msg);
+    });
+
+    socket.on('typing', function(typing){
+        console.log('Någon skriver..' + typing);
+        io.emit('typing', typing);
+    });
 });
 
 http.listen(1337, function(){
